0861c64d5d7fe53faa8a27228fee82a8c9b198e5
Max Geller <mxgeller@gmail.com>
2023-09-03 15:44:02 -0700

    removing firebase adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,16 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import firebase from "svelte-adapter-firebase"
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-        adapter: firebase()
+        adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: null
+		}),
 	}
 };
 
