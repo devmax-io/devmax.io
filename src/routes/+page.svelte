3fb9d79ac8ae62091a1ba8ed49c505424bfa1ded
Max Geller <mxgeller@gmail.com>
2023-09-03 14:11:41 -0700

    configuring tailwind.css

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,12 @@
-<h1>DevMax.io</h1>
+
+<style lang="postcss">
+	:global(html) {
+	  background-color: theme(colors.gray.100);
+	}
+  </style>
+<h1 class="text-3xl font-bold underline">
+	DevMax.io
+  </h1>
 <p>
 	Visit
 	<a href="https://kit.svelte.dev">kit.svelte.dev</a>
