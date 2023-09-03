238857a9e63a139e725e590b6b5384923c89f65f
Max Geller <mxgeller@gmail.com>
2023-09-03 14:22:06 -0700

    configuring daisyui

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,6 @@
 	:global(html) {
 	  background-color: theme(colors.gray.100);
 	}
-  </style>
-<h1 class="text-3xl font-bold underline">
-	DevMax.io
-  </h1>
-<p>
-	Visit
-	<a href="https://kit.svelte.dev">kit.svelte.dev</a>
-	to read the documentation
-</p>
+</style>
+<h1 class="text-3xl font-bold underline">DevMax.io</h1>
+<button class="btn">Hello daisyUI</button>
